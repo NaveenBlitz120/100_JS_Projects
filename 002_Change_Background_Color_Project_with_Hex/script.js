895cd37e44283fa8d0b5089bdd307ada1f17c633
Naveen-Prasad-T-Maersk <85668227+Naveen-Prasad-T-Maersk@users.noreply.github.com>
2022-03-20 02:30:20 +0530

    002_Change_Background_Color_Project_with_Hex completed

---
+++ 002_Change_Background_Color_Project_with_Hex/script.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('.btn');
+const body = document.querySelector('body');
+const Header1 = document.querySelector('h1');
+
+function colorChanger(){
+    button.addEventListener('click', ()=>{
+        const randomColor = '#' + (Math.floor(Math.random() * 2 ** 24)).toString(16).padStart(0, 6);
+        body.style.backgroundColor = randomColor;
+        Header1.textContent = 'Hex: '+ randomColor;       
+    });
+};
+
+colorChanger();
